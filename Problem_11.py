bcc3f03bde4350d872cb373bac384d12c91e42bb
YOGEESH K P <yogeesh793@gmail.com>
2024-12-28 09:28:38 +0530

    Add files via upload

---
+++ Problem_11.py
@@ -0,0 +1,13 @@
+"""Tuple Operation.
+* Create a tuple with 5 elements.
+* Try to modefy one of the elements. what happnes?
+* Perform slicing on a tuple to exstract the second and third element.
+* Concatenate the tuple with another tuple."""
+
+x=(1,2,3,4,5)
+print(x[1:3])
+
+t1=(2,5,6,4)
+t2=(6,5,4,21,3)
+t=t1+t2
+print(t)
